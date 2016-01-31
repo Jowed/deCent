33ba1a62ee9ff20967cf2cf575e16c6f90d0a84a
Buzz Tilford <tilford20@gmail.com>
2016-01-30 20:38:48 -0800

    editablity to landscapes human sa 1.7

--- client/template_helpers/land_card.js
+++ client/template_helpers/land_card.js
@@ -1,6 +1,66 @@
 Template.Land_Card.helpers({
-	get_owner: function(owner){
-		console.log(owner);
-		return Meteor.users.findOne(owner).profile;
-	}
+    get_owner: function(owner){
+	console.log(owner);
+	return Meteor.users.findOne(owner).profile;	},
+    can_edit: function(owner){
+	if (Meteor.userId() === owner)
+	    return true
+	else
+	    return false
+    },
+
+
+    editing: function(id) {
+	console.log(id, Session.get("editing_id"))
+	if (id === Session.get("editing_id"))
+	    return true
+	else
+	    return false
+    }
+
+})
+
+
+Template.Land_Card.events({
+    "click .edit-land_card": function(event, template){
+	console.log(template)
+	Session.set("editing_id", template.data._id)
+    }
+
+})
+
+
+
+Template.EditLand.helpers({
+    stringTags: function(tag_array)
+    {
+	return tag_array.toString()
+    }
+})
+
+Template.EditLand.events({
+    "submit .edit-land-form": function(event)
+    {
+	event.preventDefault();
+
+	var id = Session.get("editing_id")
+	console.log(id)
+	var name = event.target.name.value
+	var size = event.target.size.value
+	var address = event.target.address.value
+	var tags = event.target.tags.value.split(',')
+	console.log(id, name, size, address, tags)
+	Land.update({_id: id},
+		    {$set:
+			 {
+			     "name": name,
+			     "size": size,
+			     "address": address,
+			     "tags": tags
+			 }}
+		   )
+	Session.set("editing_id", "")
+
+
+    }
 })
